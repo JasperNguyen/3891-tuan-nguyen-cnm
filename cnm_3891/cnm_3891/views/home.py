2dc5ed24d73c5043e0535360f9e01e38f8acab52
JasperNguyen <info.jaspernguyen.conicalhats@gmail.com>
2021-10-11 09:21:39 +0700

    test django

---
+++ cnm_3891/cnm_3891/views/home.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+import os
+
+def index(request):
+    folderName = os.getcwd()
+    htmlFile = open(folderName + '\\cnm_3891\\views\\home.html', 'r')
+    html = htmlFile.read()
+    return HttpResponse(html)
